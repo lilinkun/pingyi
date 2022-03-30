56e7176ef6d026bba57fb9aa78e2bdce9ba55ee6
李果 <lilinkun@163.com>
2022-03-30 23:08:46 +0800

    添加修改密码

---
+++ app/src/main/java/com/communication/pingyi/ui/me/changepwd/ChangePwdFragment.kt
@@ -0,0 +1,26 @@
+package com.communication.pingyi.ui.me.changepwd
+
+import com.communication.pingyi.R
+import com.communication.pingyi.base.BaseFragment
+import com.communication.pingyi.databinding.FragmentChangepwdBinding
+
+/**
+ * Created by LG
+ * on 2022/3/30  22:48
+ * Description：
+ */
+class ChangePwdFragment  : BaseFragment<FragmentChangepwdBinding>(){
+
+    override fun getLayoutResId(): Int = R.layout.fragment_changepwd
+
+    override fun initView() {
+
+    }
+
+    override fun observeViewModels() {
+
+    }
+
+
+
+}
