c4513e7dce2242e8948462f51e5d1ac9191f626e
李果 <lilinkun@163.com>
2022-03-31 14:40:55 +0800

    添加修改密码

--- app/src/main/java/com/communication/pingyi/ui/me/changepwd/ChangePwdFragment.kt
+++ app/src/main/java/com/communication/pingyi/ui/me/changepwd/ChangePwdFragment.kt
@@ -1,19 +1,56 @@
 package com.communication.pingyi.ui.me.changepwd
 
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import com.communication.lib_core.checkDoubleClick
+import com.communication.lib_core.tools.EVENTBUS_CHANGE_PASSWORD_SUCCESS
+import com.communication.lib_http.base.MMKVTool
 import com.communication.pingyi.R
 import com.communication.pingyi.base.BaseFragment
 import com.communication.pingyi.databinding.FragmentChangepwdBinding
+import com.jeremyliao.liveeventbus.LiveEventBus
 
 /**
  * Created by LG
  * on 2022/3/30  22:48
  * Description：
  */
-class ChangePwdFragment  : BaseFragment<FragmentChangepwdBinding>(){
+class ChangePwdFragment : BaseFragment<FragmentChangepwdBinding>(){
 
     override fun getLayoutResId(): Int = R.layout.fragment_changepwd
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        LiveEventBus.get(
+            EVENTBUS_CHANGE_PASSWORD_SUCCESS,
+            Boolean::class.java
+        ).observe(this,{
+            if (checkDoubleClick()){
+                logoutConfirm()
+            }
+        })
+
+
+    }
+
     override fun initView() {
+
+        binding.apply {
+
+            llBack.setOnClickListener {
+                if (checkDoubleClick()){
+                    findNavController().navigateUp()
+                }
+            }
+
+            tvModifySave.setOnClickListener {
+                if (checkDoubleClick())
+                {}
+            }
+
+
+        }
 
     }
 
@@ -21,6 +58,12 @@ class ChangePwdFragment  : BaseFragment<FragmentChangepwdBinding>(){
 
     }
 
+
+
+    private fun logoutConfirm() {
+        MMKVTool.clearAll()
+        requireActivity().finish()
+    }
 
 
 }
